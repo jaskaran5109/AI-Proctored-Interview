dfda1ae37e638f7c6c8a7eed7c28e4577f9dfbab
jaskaran5109 <singhjaskaran2810@gmail.com>
2026-04-17 11:33:20 +0530

    dotenv added

--- frontend/src/services/http.ts
+++ frontend/src/services/http.ts
@@ -4,7 +4,7 @@ import { useAuthStore } from "@/store/authStore";
 
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
